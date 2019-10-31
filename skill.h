eb61cc9613f0c20d850cbb87e8b7526b9feb72f9
Neptune3X <52845093+Neptune3X@users.noreply.github.com>
2019-10-31 19:28:15 +0700

    Add files via upload
    ADT buat queue skill

---
+++ skill.h
@@ -0,0 +1,76 @@
+/* File : queue.h */
+/* Definisi ADT Queue dengan representasi array secara eksplisit dan alokasi dinamik */
+/* Model Implementasi Versi III dengan circular buffer */
+
+#ifndef __skill_H_
+#define __skill_H_
+
+#include "boolean.h"
+
+#define Zero 0
+/* Konstanta untuk mendefinisikan address tak terdefinisi */
+
+/* Definisi elemen dan address */
+typedef char infotype;
+typedef int address;   /* indeks tabel */
+/* Contoh deklarasi variabel bertype Queue : */
+/* Versi I : tabel dinamik, Head dan Tail eksplisit, ukuran disimpan */
+typedef struct { infotype * T;   /* tabel penyimpan elemen */
+                 address HEAD;  /* alamat penghapusan */
+                 address TAIL;  /* alamat penambahan */
+                 int MaxEl;     /* Max elemen queue */
+               } Queue;
+/* Definisi Queue kosong: HEAD=Nil; TAIL=Nil. */
+/* Catatan implementasi: T[0] tidak pernah dipakai */
+
+/* ********* AKSES (Selektor) ********* */
+/* Jika Q adalah Queue, maka akses elemen : */
+#define Head(Q) (Q).HEAD
+#define Tail(Q) (Q).TAIL
+#define InfoHead(Q) (Q).T[(Q).HEAD]
+#define InfoTail(Q) (Q).T[(Q).TAIL]
+#define MaxEl(Q) (Q).MaxEl
+
+/* ********* Prototype ********* */
+boolean IsEmptyQueue (Queue Q);
+/* Mengirim true jika Q kosong: lihat definisi di atas */
+boolean IsFullQueue (Queue Q);
+/* Mengirim true jika tabel penampung elemen Q sudah penuh */
+/* yaitu mengandung elemen sebanyak MaxEl */
+int NBElmtQueue (Queue Q);
+/* Mengirimkan banyaknya elemen queue. Mengirimkan 0 jika Q kosong. */
+
+/* *** Kreator *** */
+void CreateEmptyQueue (Queue * Q, int Max);
+/* I.S. sembarang */
+/* F.S. Sebuah Q kosong terbentuk dan salah satu kondisi sbb: */
+/* Jika alokasi berhasil, Tabel memori dialokasi berukuran Max+1 */
+/* atau : jika alokasi gagal, Q kosong dg MaxEl=0 */
+/* Proses : Melakukan alokasi, membuat sebuah Q kosong */
+
+/* *** Destruktor *** */
+void DeAlokasiQueue(Queue * Q);
+/* Proses: Mengembalikan memori Q */
+/* I.S. Q pernah dialokasi */
+/* F.S. Q menjadi tidak terdefinisi lagi, MaxEl(Q) diset 0 */
+
+/* *** Primitif Add/Delete *** */
+void AddSkillQueue (Queue * Q, infotype X);
+/* Proses: Menambahkan X pada Q dengan aturan FIFO */
+/* I.S. Q tidak kosong, tabel penampung elemen Q TIDAK penuh */
+/* F.S. X menjadi TAIL yang baru, TAIL "maju" dengan mekanisme circular buffer */
+/* Skill yang tidak bisa ditambah adalah Instant Upgrade */
+
+void DelSkillQueue (Queue * Q, infotype * X);
+/* Proses: Menghapus X pada Q dengan aturan FIFO */
+/* I.S. Q tidak mungkin kosong */
+/* F.S. X = nilai elemen HEAD pd I.S., HEAD "maju" dengan mekanisme circular buffer; 
+        Q mungkin kosong */
+
+void StartSkill(Queue *Q);
+/* Proses : Memasukkan skill pertama ke dalam Queue yang sudah disiapkan*/
+/* I.S. Q kosong*/
+/* F.S Info(First(*Q)) = "Instant_Upgrade" dan NBElmtQueue = 1*/
+
+
+#endif
