2083849b908ab0307110423be83702f54b942543
hengkysuryaa <hengkysurya88@gmail.com>
2019-11-19 22:04:32 +0700

    skill fix

--- skill.h
+++ skill.h
@@ -2,23 +2,24 @@
 /* Definisi ADT Queue dengan representasi array secara eksplisit dan alokasi dinamik */
 /* Model Implementasi Versi III dengan circular buffer */
 
-#ifndef __skill_H_
-#define __skill_H_
+#ifndef skill_H
+#define skill_H
 
 #include "boolean.h"
 
 #define Zero 0
+#define MaxQueue 10
 /* Konstanta untuk mendefinisikan address tak terdefinisi */
 
 /* Definisi elemen dan address */
-typedef int infotype;
+typedef char infotype;
 typedef int address;   /* indeks tabel */
 /* Contoh deklarasi variabel bertype Queue : */
 /* Versi I : tabel dinamik, Head dan Tail eksplisit, ukuran disimpan */
 typedef struct { infotype * T;   /* tabel penyimpan elemen */
                  address HEAD;  /* alamat penghapusan */
                  address TAIL;  /* alamat penambahan */
-                 int MaxEl;     /* Max elemen queue */
+                 int MaxElQueue;     /* Max elemen queue */
                } Queue;
 /* Definisi Queue kosong: HEAD=Nil; TAIL=Nil. */
 /* Catatan implementasi: T[0] tidak pernah dipakai */
@@ -29,35 +30,31 @@ typedef struct { infotype * T;   /* tabel penyimpan elemen */
 #define Tail(Q) (Q).TAIL
 #define InfoHead(Q) (Q).T[(Q).HEAD]
 #define InfoTail(Q) (Q).T[(Q).TAIL]
-#define MaxEl(Q) (Q).MaxEl
+#define MaxElQueue(Q) (Q).MaxElQueue
 
 /* ********* Prototype ********* */
 boolean IsEmptyQueue (Queue Q);
 /* Mengirim true jika Q kosong: lihat definisi di atas */
 boolean IsFullQueue (Queue Q);
 /* Mengirim true jika tabel penampung elemen Q sudah penuh */
-/* yaitu mengandung elemen sebanyak MaxEl */
+/* yaitu mengandung elemen sebanyak MaxElQueue */
+/* yaitu mengandung elemen sebanyak MaxElQueue */
 int NBElmtQueue (Queue Q);
 /* Mengirimkan banyaknya elemen queue. Mengirimkan 0 jika Q kosong. */
 
 /* *** Kreator *** */
-void CreateEmptyQueue (Queue * Q, int Max);
+void CreateEmptyQueue (Queue * Q);
 /* I.S. sembarang */
 /* F.S. Sebuah Q kosong terbentuk dan salah satu kondisi sbb: */
 /* Jika alokasi berhasil, Tabel memori dialokasi berukuran Max+1 */
-/* atau : jika alokasi gagal, Q kosong dg MaxEl=0 */
+/* atau : jika alokasi gagal, Q kosong dg MaxElQueue=0 */
 /* Proses : Melakukan alokasi, membuat sebuah Q kosong */
 
 /* *** Destruktor *** */
 void DeAlokasiQueue(Queue * Q);
 /* Proses: Mengembalikan memori Q */
 /* I.S. Q pernah dialokasi */
-/* F.S. Q menjadi tidak terdefinisi lagi, MaxEl(Q) diset 0 */
-
-/* *** Primitif Add/Delete *** */
-infotype SkillChecker(char X[25]);
-
-char readSkill(Queue *Q);
+/* F.S. Q menjadi tidak terdefinisi lagi, MaxElQueue(Q) diset 0 */
 
 void AddSkillQueue (Queue * Q, char Y);
 /* Proses: Menambahkan X pada Q dengan aturan FIFO */
@@ -76,5 +73,6 @@ void StartSkill(Queue *Q);
 /* I.S. Q kosong*/
 /* F.S Info(First(*Q)) = "Instant_Upgrade" dan NBElmtQueue = 1*/
 
+void KodeToSkill(char S);
 
 #endif
