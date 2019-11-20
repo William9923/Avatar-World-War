7bb2b293f2f3523ee6c13e85421988e03f1ed682
William <williamong1400@gmail.com>
2019-11-20 20:23:37 +0700

    Prosedur Skill

---
+++ prosedur_skill.h
@@ -0,0 +1,15 @@
+#include"bangunan.h"
+#include"array.h"
+#include"pemain.h"
+#include "listlinier.h"
+
+#ifndef SKILL_P
+#define SKILL_P
+
+void InstantUpgrade(TabBangunan * tab, Pemain p1);
+void TambahPasukanB(Bangunan * bangunan, int n);
+void KurangPasukan(Bangunan * bangunan, int n);
+void InstantReinforcement(TabBangunan * tab, Pemain p1);
+void Barrage(TabBangunan * tab, Pemain p1, Pemain p2);
+
+#endif
