99c4dcb864e7c9bb235e70a3e9042f1e1f31e72f
ariefdarmawant <ariefdarmawantantriady@gmail.com>
2019-11-20 11:15:00 +0700

    turn udah bener harusnya

--- utility_arief.c
+++ utility_arief.c
@@ -35,37 +35,6 @@
 #include"boolean.h"
 
 
-
-void PemainNow(Pemain P1,Pemain P2,Pemain *P,boolean swapTurn,int turn){
-	if(swapTurn){
-		//genap jadi P1, ganjil P2
-		if(turn%2==0){
-			*P = P1;
-		}
-		else{
-			*P = P2;
-		}
-	}
-	else{
-		//ganjil P1, genap P2
-		if(turn%2==0){
-			*P = P2;
-		}
-		else{
-			*P = P1;
-		}
-	}
-}
-
-void ChangeTurnOrder(boolean *swapTurn){
-	if(*swapTurn){
-		*swapTurn = false;
-	}
-	else{
-		*swapTurn = true;
-	}
-}
-
 boolean IsEQPemain(Pemain P1, Pemain P2){
 	boolean same = true;
 
@@ -75,6 +44,25 @@ boolean IsEQPemain(Pemain P1, Pemain P2){
 	return same;
 }
 
+
+void NextPemain(Pemain P1,Pemain P2,Pemain *P){
+	if(IsEQPemain(P1,*P)){
+		*P = P2;
+	}
+	else{
+		*P = P1;
+	}
+}
+
+// void ChangeTurnOrder(boolean *swapTurn){
+// 	if(*swapTurn){
+// 		*swapTurn = false;
+// 	}
+// 	else{
+// 		*swapTurn = true;
+// 	}
+
+
 int main() {
 	//KAMUS
 	Pemain P1,P2,Pnow;
@@ -83,11 +71,7 @@ int main() {
     TabBangunan AllBangunan;
     Graph connectivity;
 	List Netral;
-	int turn=1,nomor=1;
-	boolean swapTurn = false;
-
-	nomor++;
-
+	int turn=1;
 	//Baca Konfigurasi Permainan
 	printf("Reading Configuration File...\n");
     CreateEmptyList(&Netral);
@@ -101,74 +85,85 @@ int main() {
 	SaveState(&SU,P1,P2,Netral,AllBangunan);
 	P1.nomor = 1;
 	P2.nomor = 2;
-
+	Pnow = P1;
 	while(!stop){
 		//Validasi Command
 		//Print Map
-		printf("%s%d%s\n", "**************TURN ", turn , "**************");
-		PemainNow(P1,P2,&Pnow,swapTurn,turn);
-		CetakPeta(P,P1,P2,AllBangunan);
-		//Baca Command taruh disini...
-		//...
-		printf("Player %d\n",Pnow.nomor);
-		PrintInfoLBangunan(AllBangunan,Pnow);
-		//printskill
-		printf("ENTER COMMAND: ");
-		s = BacaInputUser();
-		printf("\n");
-		if(IsAttack(s)){
-			if (IsEQPemain(Pnow, P1)) {
-				ProsedurAttack(&AllBangunan, &P1, &P2,&Netral ,connectivity);
-			} else {
-				// pemain p2
-				ProsedurAttack(&AllBangunan, &P2, &P1,&Netral ,connectivity);
-			}
-			endgame(P1,P2,&stop);
-		}
-		else if(IsLevelUp(s)){
-			if (IsEQPemain(Pnow, P1)) {
-				ProsedurLevelUp(&AllBangunan,P1);
-			} else {
-				// pemain p2
-				ProsedurLevelUp(&AllBangunan,P2);
-			}
-		}
-		else if(IsMove(s)){
-			if (IsEQPemain(Pnow, P1)){
-				ProsedurMove(&AllBangunan, &P1, connectivity);
-			} else {
-				// pemain p2
-				ProsedurMove(&AllBangunan, &P2, connectivity);
-			}
-		}
-		else if(IsSkill(s)){
-			// nanti skillnya tuh sesuain aja ama yang lain, 
-			// trus setiap skill tuh bakal jadi prosedur, dimana dia bakal ngambil dari inputan,
-			// trus output hasilnya.
-		}
-		else if(IsUndo(s)){
-			LoadState(&SU,&P1,&P2,&Netral,&AllBangunan);
-		}
-		else if(IsEndTurn(s)){
-			if (turn > 1){
-				if (IsEQPemain(Pnow, P2)){
-					NextTurnLBangunan(P1.b, &AllBangunan);
+		int jatah=1;
+		while(!stop && jatah!=0){
+			printf("%s%d%s\n", "**************TURN ", turn , "**************");
+			CetakPeta(P,P1,P2,AllBangunan);
+			//Baca Command taruh disini...
+			//...
+			printf("Player %d\n",Pnow.nomor);
+			PrintInfoLBangunan(AllBangunan,Pnow);
+			//printskill
+			printf("ENTER COMMAND: ");
+			s = BacaInputUser();
+			printf("\n");
+			if(IsAttack(s)){
+				if (IsEQPemain(Pnow, P1)) {
+					ProsedurAttack(&AllBangunan, &P1, &P2,&Netral ,connectivity);
 				} else {
-					// P1 sekarang
-					NextTurnLBangunan(P2.b, &AllBangunan);
+					// pemain p2
+					ProsedurAttack(&AllBangunan, &P2, &P1,&Netral ,connectivity);
+				}
+				endgame(P1,P2,&stop);
+			}
+			else if(IsLevelUp(s)){
+				if (IsEQPemain(Pnow, P1)) {
+					ProsedurLevelUp(&AllBangunan,P1);
+				} else {
+					// pemain p2
+					ProsedurLevelUp(&AllBangunan,P2);
 				}
 			}
-			turn++;
-			system("clear");
+			else if(IsMove(s)){
+				if (IsEQPemain(Pnow, P1)){
+					ProsedurMove(&AllBangunan, &P1, connectivity);
+				} else {
+					// pemain p2
+					ProsedurMove(&AllBangunan, &P2, connectivity);
+				}
+			}
+			else if(IsSkill(s)){
+				// nanti skillnya tuh sesuain aja ama yang lain, 
+				// trus setiap skill tuh bakal jadi prosedur, dimana dia bakal ngambil dari inputan,
+				// trus output hasilnya.
+			}
+			else if(IsUndo(s)){
+				if(!IsEmptyStack(SU)){
+					LoadState(&SU,&P1,&P2,&Netral,&AllBangunan);
+				}
+				else{
+					printf("Tidak ada yang bisa di undo!.\n");
+				}
+				
+			}
+			else if(IsEndTurn(s)){
+				if (turn > 1){
+					if (IsEQPemain(Pnow, P2)){
+						NextTurnLBangunan(P1.b, &AllBangunan);
+					} else {
+						// P1 sekarang
+						NextTurnLBangunan(P2.b, &AllBangunan);
+					}
+				}
+				turn++;
+				jatah--;
+				system("clear");
+			}
+			else if(IsSave(s)){
+				//DISINI COBA CODING EXTRA TURN
+				jatah++;
+			}
+			else if(IsExit(s)) {
+				stop = true;
+			}
+			else{
+				printf("Inputan tidak valid.\n");
+			}
 		}
-		else if(IsSave(s)){
-
-		}
-		else if(IsExit(s)) {
-			stop = true;
-		}
-		else{
-			printf("Inputan tidak valid.\n");
-		}
+		NextPemain(P1,P2,&Pnow);
 	}
 }
