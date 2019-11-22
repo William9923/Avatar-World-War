0038382035b91eb2c3bae05c539d2cac930e4b60
hengkysuryaa <hengkysurya88@gmail.com>
2019-11-22 10:22:28 +0700

    art start game

---
+++ art.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+
+
+void StartGame();
+
+int main () 
+{
+    StartGame();
+    return 0;
+}
+void StartGame ()
+{
+
+char anykey;
+ 
+printf("\n");
+printf("    |>>>                                                      |>>> \n");
+printf("    |                                                         |    \n");
+printf("    *               WELCOME TO AVATAR: WORLD WAR              *    \n");
+printf("  [ I ]                                                     [ I ]  \n");
+printf("  [ F ]                   |>>>          |>>>                [ F ]  \n");
+printf("  [   ]_ _ _              |             |              _ _ _[   ]  \n");
+printf("  [   ]------|       {#########}   {#########}       |----- [   ]  \n");
+printf("  [ I ]====/          |=======|     |=======|         /==== [ I ]  \n");
+printf("  [ F ]    |           |     |_ _ _ _|     |           |    [ F ]  \n");
+printf("  [___]    |_ _ _ _ _ _|     | X X X |     |_ _ _ _ _ _|    [___]  \n");
+printf("   |   T   |A V A T A R|     |=======|     |  G A M E  |   U   |  \n");
+printf("   |   A   |===========|     | + ^ + |     |===========|   D   |   \n");
+printf("   |   N   |           |  A  |_______|  A  |           |   A   |    \n");
+printf("   |   A   |           |  I  |||||||||  P  |           |   R   |    \n");
+printf("   |   H   |           |  R  ||vvvvv||  I  |           |   A   |    \n");
+printf("_-_|_______|-----------|_____||     ||_____|-----------|_______|_-_\n");
+printf("  |_________|         |______||     ||______|         |_________|    \n");
+printf(" |___________|-------|_______||_____||_______|-------|___________|   \n");
+printf("\n");
+printf("     *                 PRESS ANY KEY TO START                *    \n");
+scanf("%c", &anykey);
+
+}
